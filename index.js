05fa02bc4e2d95f50a2811e47c15250f6773160f
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-05-09 21:59:19 +0300

    basic implementation

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-const aria = require("aria-query");
+"use strict";
 
-const elementRoles = {};
-aria.elementRoles.forEach((value, { name }) => {
-  elementRoles[name] = Array.from(value);
-});
-const areas = {};
-aria.aria.forEach((value, name) => {
-    areas[name] = value;
-});
-function isAttributeValue(focusPath) {
-    return  focusPath.node.type === "TextNode" &&
-    focusPath.parent.type === "AttrNode";
-}
-module.exports.onComplete = function(_, { focusPath, results, type }) {
-  if (type !== "template") {
-    return results;
-  }
-  if (
-    isAttributeValue(focusPath)
-  ) {
-    let attributeName = focusPath.parent.name;
-    let element = focusPath.parentPath.parent;
-    if (element.tag in elementRoles && attributeName === "role") {
-      elementRoles[element.tag].forEach(name => {
-        results.push({
-          label: name
-        });
-      });
-    } else if (attributeName in areas && areas[attributeName].type === 'tokenlist') {
-        areas[attributeName].values.forEach(name => {
-            results.push({
-              label: name
-            });
-        });
-    }
-  }
+const { TextEdit, Position } =  require('vscode-languageserver');
+const { URI } =  require('vscode-uri');
 
-  return results;
+module.exports = {
+  onInit(_, project) {
+
+    project.executors['els.extractSourceCodeToComponent'] = async (server, filePath, [componentName, { range, source, uri }]) => {
+      try {
+        // const ast = server.templateCompletionProvider.getAST(document.getText(range));
+        // const focusPath = server.templateCompletionProvider.createFocusPath(ast, ast.loc.start, text);
+        await server.onExecute({
+          command: 'els.executeInEmberCLI',
+          arguments: [filePath, `g component ${componentName}`]
+        });
+        // going to wait for file changes api
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        const registry = server.getRegistry(project.root);
+        if (!(componentName in registry.component)) {
+          console.log(`Unable to find component ${componentName} in registry ${JSON.stringify(registry.component)}`);
+          return;
+        }
+        const fileName = registry['component'][componentName].find((file) => file.endsWith('.hbs'));
+        if (!fileName) {
+          console.log(`Unable to find template file for component ${componentName}`);
+          return;
+        }
+        const fileUri = URI.file(fileName).toString();
+        const edit = {
+          changes: {
+            [uri]: [TextEdit.replace(range, `<${normalizeToAngleBracketComponent(componentName)} />`)],
+            [fileUri]: [TextEdit.insert(Position.create(0, 0), source)]
+          }
+        };
+        await server.connection.workspace.applyEdit(edit);
+      } catch (e) {
+        logError(e);
+      }
+    };
+  }
 };
